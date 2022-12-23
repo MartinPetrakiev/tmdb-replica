e51b6683068c694c65de0a6bc290502fafe1c2d9
Martin Petrakiev <mpetrakiev@jtn.internal>
2022-12-24 00:38:29 +0200

    landing page and login

--- client/src/components/AuthContainer.jsx
+++ client/src/components/AuthContainer.jsx
@@ -1,9 +1,55 @@
 import React from 'react'
+import styles from '../styles/Login.module.scss';
+import userPng from '../styles/assets/user.png';
+import { TextField, Box, Button, Avatar } from '@mui/material';
+
+function handleSubmit(e) {
+  e.preventDefault()
+  console.log(e.target)
+  console.log(process.env.REACT_APP_TMDB_API_KEY);
+}
 
 function AuthContainer() {
   return (
-    <div>AuthContainer</div>
-  )
+    <div className={styles.container}>
+      <Avatar sx={{ width: 100, height: 100, margin: '10px' }} alt="User" src={userPng} />
+      <Box
+        className={styles.form}
+        component="form"
+        onSubmit={handleSubmit}
+        sx={{
+          '& .MuiTextField-root': {
+            m: '5px',
+            width: '300px'
+          },
+        }}
+      >
+        <TextField label="Email address" type="email" name="email" variant="filled" sx={{
+          '.MuiInputLabel-root': {
+            color: '#01b4e4'
+          },
+          '.MuiInputLabel-root.Mui-focused': {
+            color: '#1ed5a9'
+          },
+          '.MuiFilledInput-root': {
+            color: '#d2d2d2'
+          },
+        }} />
+        <TextField label="Password" type="password" name="password" variant="filled" sx={{
+          '.MuiInputLabel-root': {
+            color: '#1ed5a9'
+          },
+          '.MuiInputLabel-root.Mui-focused': {
+            color: '#01b4e4'
+          },
+          '.MuiFilledInput-root': {
+            color: '#d2d2d2'
+          },
+        }}/>
+        <Button className={styles.btn_submit} type='submit'>Submit</Button>
+      </Box>
+    </div>
+  );
 }
 
 export default AuthContainer
--- client/src/components/LandingPage.jsx
+++ client/src/components/LandingPage.jsx
@@ -1,21 +1,43 @@
-import React from 'react'
-import { useNavigate } from "react-router-dom";
+import React, { useState } from 'react'
+// import { useNavigate } from "react-router-dom";
 import styles from '../styles/LandingPage.module.scss';
+import AuthContainer from './AuthContainer'
+
 
 function LandingPage() {
-    const navigate = useNavigate();
+    const [componentStyles, setComponentStyles] = useState({
+        image: {
+            flexGrow: 0.7
+        },
+        hc_container: {
+            flexGrow: 0.3,
+        }
+    });
+    const [authShow, setAuthShow] = useState(false);
+
+    const redirectToLogin = (event) => {
+        event.preventDefault()
+        setComponentStyles({image: { flexGrow: 0 }, hc_container: { flexGrow: 1}})
+        setAuthShow(true)
+    }
+
 
     return (
-        <div className={styles.container}>
-            <div className={styles.heading_card_wrapper}>
-                <div className={styles.hc_container}>
-                    <h1>TMDB Replica</h1>
-                    <p>Find what to watch next among millions of Movies and TV Shows. Enter now.</p>
-                    <div className={styles.buttons_container}>
-                        <button onClick={() => navigate('/auth', { tabValue: 0 })} className={styles.button1}>LOGIN</button>
-                        <button onClick={() => navigate('/auth', { tabValue: 1 })} className={styles.button2}>REGISTER</button>
-                    </div>
-                </div>
+        <div className={styles.wrapper}>
+            <div className={styles['image-container']} style={componentStyles.image} />
+            <div className={styles.hc_container} style={componentStyles.hc_container}>
+                {authShow
+                    ? (<AuthContainer />)
+                    : (
+                        <div className={styles['welcome-container']}>
+                            <h1>TMDB Replica</h1>
+                            <p>Find what to watch next among millions of Movies and TV Shows. Enter now.</p>
+                            <div className={styles.buttons_container}>
+                                <button className={styles.button1} onClick={redirectToLogin}>Explore now</button>
+                            </div>
+                        </div>
+                    )
+                }
             </div>
         </div>
     );
